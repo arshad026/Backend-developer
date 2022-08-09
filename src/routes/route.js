f6051cffdebac031a538585717e8c15c99a5e24e
Arsh <mr.arsh026@gmail.com>
2022-08-10 00:17:36 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
@@ -9,11 +10,88 @@ router.get('/test-me', function (req, res) {
 });
 
 
-router.get('/test-you', function(req, res){
+router.get('/test-you', function (req, res) {
     res.send('This is the second routes implementation')
 })
 
-router.get('/give-me-students-data',function(req, res){
+
+
+router.get('/movie', function (req, res) {
+    let movie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movie)
+})
+
+
+
+router.get('/movies/:indexNumber', function (req, res) {
+    let movie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(req.params.indexNumber)
+    let i = req.params.indexNumber
+    if (i < 0 || i >= movie.length) {
+        return res.send('The index value is incorrect')
+    }
+    let result = movie[i]
+    res.send(result)
+})
+
+
+
+router.get('/film', function (req, res) {
+    let arr = [
+        {
+            'id': 1,
+            'name': 'Rang de basanti'
+        },
+        {
+            'id': 2,
+            'name': 'The shining'
+        },
+        {
+            'id': 3,
+            'name': 'Lord of the rings'
+        },
+        {
+            'id': 4,
+            'name': 'Batman begins'
+        }
+    ]
+    res.send(arr)
+})
+
+
+
+router.get('/films/:filmId', function (req, res) {
+    let filmName = [
+        {
+            'id': 1,
+            'name': 'The Shining'
+        },
+        {
+            'id': 2,
+            'name': 'Incendies'
+        },
+        {
+            'id': 3,
+            'name': 'Rang de Basanti'
+        },
+        {
+            'id': 4,
+            'name': 'Finding Nemo'
+        }
+    ]
+    let filmId = req.params.filmId
+
+    for (i = 0; i < filmName.length; i++) {
+        let film = filmName[i]
+        if (film.id == filmId) {
+            return res.send(film)
+        }
+    }
+    res.send("The film id doesn't match any movie")
+})
+
+
+router.get('/give-me-students-data', function (req, res) {
 
 })
 module.exports = router;
