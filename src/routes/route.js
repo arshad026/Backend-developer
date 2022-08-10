6ecfb024f920a1450685b399d38dd5e1b40263f8
Arsh <mr.arsh026@gmail.com>
2022-08-10 20:10:07 +0530

    post-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,4 @@
-const { application } = require('express');
+const { query } = require('express');
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
@@ -6,52 +6,62 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
-    logger.welcome()
-
     res.send('My second ever api!')
 });
 
 
+let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
 
 
-router.get('/cohort-member', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+router.post('/players-name', function (req, res) {
+    let player = req.body
+    let playerName = req.body.name
+    let isNameRepeated = false
+
+    for (i = 0; i < players.length; i++) {
+        if (players[i].name == playerName) {
+            isNameRepeated = true
+            break;
+        }
+    }
+    if(isNameRepeated){
+        res.send ('This player was already added!')
+    }
+    else{
+        players.push(player)
+        res.send(players)
+
+    }
 })
 
-router.get('/students-details/Sabiha', function (req, res){
-    let details = "Sabiha Khan"
-    res.send (details)
-})
-
-router.get('/students-details/:name', function (req, res){
-    console.log("This is the request" + JSON.stringify(req.params))
-    let reqParams = req.params
-    let studentsName = reqParams.name
-    console.log('Name of the students is', studentsName)
-    let studentDetails = studentsName + " " + studentsName
-    
-    res.send (studentDetails)
-})
-
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
 
 module.exports = router;
+// adding this comment for no reason
