dab44a57b3b79d549f23b98f91131db25eec2a68
Arsh <mr.arsh026@gmail.com>
2022-08-14 11:10:08 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
+const { default : mongoose } = require('mongoose');
 const route = require('./routes/route.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://arsh26:rn43mNePN23BJZwy@cluster0.24hhkez.mongodb.net/arsh-26?retryWrites=true&w=majority", {
+    useNewUrlParser : true
+})
+.then( () => console.log("MongoDB is connected"))
+.catch ( err => console.log(err) )
+
 
 app.use('/', route);
 
