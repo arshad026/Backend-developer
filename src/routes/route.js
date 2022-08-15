5b9a0829e0428b195ea98edc19b7045a3f329c51
Arsh <mr.arsh026@gmail.com>
2022-08-15 23:57:30 +0530

    test-me

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const bookController = require("../controllers/bookController")
 
 
+router.get('/test-me', function(req, res){
+    res.send("My first api!")
+})
+
+
 router.post('/createBook', bookController.createBook)
 
 router.get('/getBookData', bookController.getBookData)
